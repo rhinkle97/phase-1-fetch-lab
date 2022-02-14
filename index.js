a31f87a224c07219fbcc9c32d94f78d3d4af88e4
Ryan Hinkle <hinkle.ryan97@gmail.com>
2022-02-14 09:58:55 -0800

    commit tree 6465a1d93a9d2c9ee72c2c7756d51cd3ac1013b7

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
+
+function fetchBooks(books) {
+  return books;
+}
+
+
+
+// function fetchBooks() {
+//   // To pass the tests, don't forget to return your fetch!
   
-}
+// }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
